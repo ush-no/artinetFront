f6d13d851f7766031900c666b92c84ea9c3506e2
Ushno <ushno.loud@mail.com>
2024-05-14 22:38:44 +0600

    new app

---
+++ front-end/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Index from './Index.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Index />
+    <h1 className="text-3xl font-bold underline">
+    </h1>
+  </React.StrictMode>,
+)
